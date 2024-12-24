a0531ab07c19880c599eed61bb72a8f8c6d3f38d
sakibhasansufi <sakibhasansufi@gmail.com>
2024-12-25 02:12:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRouter from './routes/auth.route.js';
+
+import { connectDB } from './lib/db.js';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/api/auth', authRouter);
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+});
