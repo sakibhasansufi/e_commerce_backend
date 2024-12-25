f0531d6bf62670c7f1f3c78c0113c3daa53d4a56
sakibhasansufi <sakibhasansufi@gmail.com>
2024-12-26 00:57:18 +0600

    user model has been added

---
+++ models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email : {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password : {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"]
+    },
+    cartItems : [
+        {
+            quantity: {
+                type: Number,
+                default: 1
+            },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product"
+            }
+        }
+    ],
+    role: {
+        type: String,
+        enum: ["customer", "admin"],
+        default: "customer"
+    }
+}, {timestamps: true});
+
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        next();
+    };
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePasswords = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
