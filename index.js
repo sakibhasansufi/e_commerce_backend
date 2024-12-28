0531dcbb6e4ed87d3a95de2edf84b46e5267f93b
sakibhasansufi <sakibhasansufi@gmail.com>
2024-12-28 21:57:07 +0600

    redis controller just getting started

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/auth.route.js';
+import productRouter from './routes/product.route.js';
 
 import { connectDB } from './lib/db.js';
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
+app.use('/api/product', productRouter);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
