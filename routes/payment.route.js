5f0d59ad55f459b6f8c16565d0f8e289d7649be5
sakibhasansufi <sakibhasansufi@gmail.com>
2025-01-30 01:46:11 +0600

    stripe backend is completed

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { createCheckOutSession } from "../controllers/payment.controller.js";
+import { checkOutSuccess, createCheckOutSession } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
 router.post('/create-payment-checkout',protectRoute,createCheckOutSession);
+router.post('/checkout-success',protectRoute,checkOutSuccess);
 
 
 export default router;
